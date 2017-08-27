67b2158efa1db87ec9cfa95c44cefecfe31ec8ce
joe Developer <joe@dev.org>
2017-08-27 12:11:18 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by stephane yantio on 8/27/17.
+//  Copyright © 2017 stephane yantio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by stephane yantio on 8/27/17.
+//  Copyright © 2017 stephane yantio. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
